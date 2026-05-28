847124279edcc1c93455bec2269278ad66634b1b
GET Academy <terje@kolderup.net>
2026-05-28 10:21:56 +0200

    x

---
+++ FlexibleList/MyFlexibleList.cs
@@ -0,0 +1,35 @@
+﻿namespace FlexibleList
+{
+    internal class MyFlexibleList
+    {
+        private string[] _texts;
+        private int _index;
+
+        public MyFlexibleList()
+        {
+            _texts = new string[4];
+            _index = 0;
+        }
+
+        public void Add(string text)
+        {
+            if (_index >= _texts.Length)
+            {
+                var newTexts = new string[_texts.Length * 2];
+                Array.Copy(_texts, newTexts, _texts.Length);
+                _texts = newTexts;
+            }
+
+            _texts[_index] = text;
+            _index++;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < _index; i++)
+            {
+                Console.WriteLine($"På index {i} ligger teksten: {_texts[i]}");
+            }
+        }
+    }
+}
---
+++ FlexibleList/Program.cs
@@ -0,0 +1,35 @@
+﻿using FlexibleList;
+
+var list = new MyFlexibleList();
+while (true)
+{
+    Console.Write("Skriv noe: ");
+    var text = Console.ReadLine();
+    list.Add(text);
+    list.Show();
+}
+
+//var texts = new string[4];
+//int index = 0;
+
+//while (true)
+//{
+//    Console.Write("Skriv noe: ");
+//    var text = Console.ReadLine();
+
+//    if (index >= texts.Length)
+//    {
+//        var newTexts = new string[texts.Length * 2];
+//        Array.Copy(texts, newTexts, texts.Length);
+//        texts = newTexts;
+//    }
+
+//    texts[index] = text;
+//    index++;
+
+//    // Vise hele arrayen
+//    for (int i = 0; i < texts.Length; i++)
+//    {
+//        Console.WriteLine($"På index {i} ligger teksten: {texts[i]}");
+//    }
+//}
